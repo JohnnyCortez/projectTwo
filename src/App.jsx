2222b9f91196e68e81c8dfd7e6a82423e43433ec
Johnny Cortez <johnny.cortez@duke.edu>
2023-03-12 01:06:41 -0500

    began the form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import FlashCard from './Components/FlashCards';
 import { NextButton, BackButton, RandomButton }  from './Components/Buttons';
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,17 +1,32 @@
-import { useState } from 'react';
+import React, { useState, Component } from 'react';
 import '../App.css';
 
-const MainForm = (props) => {
-    return(
-        <div>
-            <form>
-                <label>Answer:
-                    <input type="text" name="name" />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-    )
-}
+class MainForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 'write your guess here'
+        };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    };
+
+    handleChange(event_) {
+        event_.preventDefault();
+    };
+
+    Render() {
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Answer:
+                        <input type="text" name="name" value={this.state.value} onChange={this.handleChange}/>
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        );
+    };
+};
 
 export default MainForm;
